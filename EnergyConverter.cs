84faea1c671e4fce6bd4dd1d95e408a446766b89
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2022-06-08 18:03:11 +0200

    Add project files.

---
+++ EnergyConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngergyLib
+{
+   public static class EnergyConverter
+    {
+        public static double ToJoule(double calorie)
+        {
+            double joule = calorie * 4.2;
+
+            return Math.Round(joule);
+        }
+
+        public static double ToCalorie(double joule)
+        {
+            double calorie = joule / 4.2;
+
+            return Math.Round(calorie);
+        }
+    }
+}
---
+++ EnergyData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EngergyLib
+{
+    public class EnergyData
+    {
+       
+
+        public int Id { get; set; }
+        public string EnergyMetric { get; set; }
+        public double Value{ get; set; }
+
+        public EnergyData()
+        {
+            
+        }
+
+        public EnergyData(string energyMetric, double value)
+        {
+            EnergyMetric = energyMetric;
+            Value = value;
+        }
+
+    }
+}
